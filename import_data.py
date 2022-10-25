3d2b4ce9eaf9191ef2d10c286918a4952dcb576c
nekoshaurman <mrrft2@gmail.com>
2022-10-25 05:19:22 +0300

    Change comments

--- import_data.py
+++ import_data.py
@@ -1,34 +1,22 @@
-#################
-## PREPARATION ##
-#################
-
-# Import modules
 import sys
 
-# If your authentification script is not in the project directory
-# append its folder to sys.path
-sys.path.append("../spotify_api_web_app")
 import authorization
 import pandas as pd
 from tqdm import tqdm
 import time
 
-# Authorize and call access object "sp"
+# Authorization
 sp = authorization.authorize()
 
 # Get all genres
 genres = sp.recommendation_genre_seeds()
 
-# Set number of recommendations per genre
+# Tracks per genre
 n_recs = 50
 
 # Initiate a dictionary with all the information you want to crawl
 data_dict = {"id": [], "genre": [], "track_name": [], "artist_name": [],
              "valence": [], "energy": []}
-
-################
-## CRAWL DATA ##
-################
 
 # Get recs for every genre
 for g in tqdm(genres):
@@ -38,13 +26,12 @@ for g in tqdm(genres):
     # json-like string to dict
     recs = eval(recs.json().replace("null", "-999").replace("false", "False").replace("true", "True"))["tracks"]
 
-    # Crawl data from each track
+    # Save meta from tracks
     for track in recs:
         # ID and Genre
         data_dict["id"].append(track["id"])
         data_dict["genre"].append(g)
 
-        # Metadata
         track_meta = sp.track(track["id"])
         data_dict["track_name"].append(track_meta.name)
         data_dict["artist_name"].append(track_meta.album.artists[0].name)
@@ -57,10 +44,7 @@ for g in tqdm(genres):
         # Wait 0.2 seconds per track so that the api doesnt overheat
         time.sleep(0.2)
 
-##################
-## PROCESS DATA ##
-##################
-
+# Pandas
 # Store data in dataframe
 df = pd.DataFrame(data_dict)
 
